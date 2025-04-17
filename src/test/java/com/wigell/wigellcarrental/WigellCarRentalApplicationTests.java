0a01388b7549ea1c8e3c2d94796de98c318f728e
aaalx <alexander.westerberg.svedberg@gmail.com>
2025-04-17 10:06:21 +0200

    first commit

---
+++ src/test/java/com/wigell/wigellcarrental/WigellCarRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wigell.wigellcarrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigellCarRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
