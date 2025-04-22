f3418f283e78f6bd9a79b49918f24994473b4088
Sara <sara.andersson185@gmail.com>
2025-04-22 15:00:52 +0200

    WIG-35-SA * Added function to remove orders before the date given, using the end_date as reference

--- src/main/java/com/wigell/wigellcarrental/services/OrderService.java
+++ src/main/java/com/wigell/wigellcarrental/services/OrderService.java
@@ -3,6 +3,7 @@ package com.wigell.wigellcarrental.services;
 import com.wigell.wigellcarrental.entities.Order;
 
 import java.security.Principal;
+import java.time.LocalDate;
 import java.util.List;
 
 //SA
@@ -10,4 +11,6 @@ public interface OrderService {
     List<Order> getActiveOrders();//SA
     List<Order> getActiveOrdersForCustomer(String personalIdentityNumber);//AWS
     String cancelOrder(Long orderId, Principal principal);//SA
+
+    String removeOrdersBeforeDate(LocalDate date, Principal principal);
 }
