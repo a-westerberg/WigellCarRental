419175cd5852e0452db60e9b9a95ce36e51ff336
Sommar-skog <amandaaronsson@hotmail.com>
2025-04-22 12:44:40 +0200

    finished InvalidInputException.java

--- src/main/java/com/wigell/wigellcarrental/exceptions/InvalidInputException.java
+++ src/main/java/com/wigell/wigellcarrental/exceptions/InvalidInputException.java
@@ -1,4 +1,33 @@
 package com.wigell.wigellcarrental.exceptions;
 
-public class InvalidInputException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+//WIG-8-AA
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidInputException extends RuntimeException {
+
+    private final String object;
+    private final String field;
+    private final Object value;
+
+    public InvalidInputException(String object, String field, Object value) {
+        super(String.format("Invalid input: %S [%s] cannot be %s.", object, field, value));
+        this.object = object;
+        this.field = field;
+        this.value = value;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
 }
