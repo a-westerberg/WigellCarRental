6503c356ee33c772405311deeb0425197cffbf56
jamtgard <jamtgardsimon@gmail.com>
2025-04-23 13:35:45 +0200

    WIG-29-SJ Done. Commit for Master->Branch merge

--- src/main/java/com/wigell/wigellcarrental/services/CustomerServiceImpl.java
+++ src/main/java/com/wigell/wigellcarrental/services/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package com.wigell.wigellcarrental.services;
 import com.wigell.wigellcarrental.entities.Customer;
 import com.wigell.wigellcarrental.exceptions.ResourceNotFoundException;
 import com.wigell.wigellcarrental.repositories.CustomerRepository;
+import com.wigell.wigellcarrental.services.utilities.MicroMethods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -36,4 +37,49 @@ public class CustomerServiceImpl implements CustomerService{
         return customerRepository.findById(id).orElseThrow(() ->
                 new ResourceNotFoundException("Customer","id",id));
     }
+
+    // WIG-29-SJ
+    @Override
+    public Customer updateCustomer(Customer customer) {
+        Customer updatedCustomer = validateCustomer(customer);
+        return customerRepository.save(updatedCustomer);
+    }
+
+    public Customer validateCustomer(Customer customer) {
+        Customer existingCustomer = customerRepository.findById(customer.getId()).orElseThrow(()->
+                new ResourceNotFoundException("Customer","id",customer.getId()));
+
+        if (MicroMethods.validateForUpdate(customer.getFirstName())) {
+            existingCustomer.setFirstName(customer.getFirstName());
+        }
+        if (MicroMethods.validateForUpdate(customer.getLastName())) {
+            existingCustomer.setLastName(customer.getLastName());
+        }
+        /*
+        // WIG-29-SJ
+        // If values of Email & Phone needs to be unique. If not, remove code later.
+        if (MicroMethods.validateForUpdate(customer.getEmail())) {
+            MicroMethods.validateUniqueValue("email", customer.getEmail(), customerRepository::existsByEmail);
+            existingCustomer.setEmail(customer.getEmail());
+        }
+
+        if (MicroMethods.validateForUpdate(customer.getPhoneNumber())) {
+            MicroMethods.validateUniqueValue("phoneNumber", customer.getPhoneNumber(), customerRepository::existsByPhoneNumber);
+            existingCustomer.setPhoneNumber(customer.getPhoneNumber());
+        }
+        */
+
+        if (MicroMethods.validateForUpdate(customer.getEmail())) {
+            existingCustomer.setEmail(customer.getEmail());
+        }
+        if (MicroMethods.validateForUpdate(customer.getPhoneNumber())) {
+            existingCustomer.setPhoneNumber(customer.getPhoneNumber());
+        }
+        if (MicroMethods.validateForUpdate(customer.getAddress())) {
+            existingCustomer.setAddress(customer.getAddress());
+        }
+
+        return existingCustomer;
+    }
+
 }
--- src/main/java/com/wigell/wigellcarrental/services/utilities/MicroMethods.java
+++ src/main/java/com/wigell/wigellcarrental/services/utilities/MicroMethods.java
@@ -1,15 +1,29 @@
 package com.wigell.wigellcarrental.services.utilities;
 
 import com.wigell.wigellcarrental.exceptions.InvalidInputException;
+import com.wigell.wigellcarrental.exceptions.UniqueConflictException;
+
 // WIG-28-SJ
 public class MicroMethods {
 
     // WIG-28-SJ
-    public static <T> T validateData(String resource, String field, T value) {
-        if (value == null) {
+    public static <T> void validateData(String resource, String field, T value) {
+        if (value == null || (value instanceof String && ((String) value).isBlank())) {
             throw new InvalidInputException(resource, field, value);
         }
-        return value;
+    }
+
+    // WIG-29-SJ
+    public static boolean validateForUpdate(Object value) {
+        return value != null && (!(value instanceof String && ((String) value).isEmpty()));
+    }
+
+    // WIG-29-SJ
+    // If values multiple values needs to be unique. If not, remove code later.
+    public static <T> void validateUniqueValue(String fieldName, T value, java.util.function.Predicate<T> existsFunction) {
+        if (value != null && existsFunction.test(value)) {
+            throw new UniqueConflictException(fieldName, value);
+        }
     }
 
 
