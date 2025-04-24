9efafe77498ac8205b505d759a5c3c162fa1a9c9
Sara <sara.andersson185@gmail.com>
2025-04-24 16:17:09 +0200

    WIG-72-76-SA * Created a Config file for security, only bare minimum right now * Took in spring.security in pom.xml * Changed the PreAuthorise so they are correctly written

---
+++ src/main/java/com/wigell/wigellcarrental/configs/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.wigell.wigellcarrental.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll())//Ändra senare till authorized
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+        //Ändra till authenticated när UserDetailsService är inlagt, ta in PreAuthorize i controller klasserna också
+    }
+}
