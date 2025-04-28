7498af624beafd43cb02392570689b3acc2ed141
Sara <sara.andersson185@gmail.com>
2025-04-28 10:43:43 +0200

    WIG-82-SA * Fixed so you can view /h2-console even with security * Have some comments oon what to do next in/with WebSecurityConfig * Had to take in a missing import

--- src/main/java/com/wigell/wigellcarrental/configs/WebSecurityConfig.java
+++ src/main/java/com/wigell/wigellcarrental/configs/WebSecurityConfig.java
@@ -8,19 +8,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+//SA
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)//Kolla om det fungerar utan
+@EnableMethodSecurity(prePostEnabled = true)//Kolla om det fungerar utan på PreAuthorize i controllers
 public class WebSecurityConfig {
 
+    //SA
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll())//Ändra senare till authorized
-                .httpBasic(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable());
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()//Ändra till admin senare
+                        .anyRequest().permitAll()//Ändra senare till authorized
+                )
+                .csrf(csrf -> csrf
+                        .disable()
+                )
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.sameOrigin())
+                )
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
         //Ändra till authenticated när UserDetailsService är inlagt, ta in PreAuthorize i controller klasserna också
     }
 }
+//.ignoringRequestMatchers("/h2-console/**")//på csrf, men den verkar inte behövas
