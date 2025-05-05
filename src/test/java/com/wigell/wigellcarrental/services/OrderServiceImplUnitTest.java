7107554fde1462b3e5f3265e91db623948b2fd3c
Sommar-skog <amandaaronsson@hotmail.com>
2025-05-05 15:01:01 +0200

    Added tests

--- src/test/java/com/wigell/wigellcarrental/services/OrderServiceImplUnitTest.java
+++ src/test/java/com/wigell/wigellcarrental/services/OrderServiceImplUnitTest.java
@@ -1,5 +1,6 @@
 package com.wigell.wigellcarrental.services;
 
+import com.wigell.wigellcarrental.enums.CarStatus;
 import com.wigell.wigellcarrental.exceptions.ResourceNotFoundException;
 import com.wigell.wigellcarrental.models.entities.Car;
 import com.wigell.wigellcarrental.models.entities.Customer;
@@ -17,8 +18,10 @@ import static org.mockito.ArgumentMatchers.any;
 
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
 import java.security.Principal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.Mockito.when;
@@ -44,33 +47,20 @@ class OrderServiceImplUnitTest {
 
 
     //AA
-    @Mock
-    Car mockCar;
-
-    //AA
-    @Mock
-    Customer mockCustomer;
-
-    //AA
-    @Mock
-    Principal mockPrincipal = () -> mockCustomer.getPersonalIdentityNumber();
-
-    //AA
-    private Order testOrder = new Order();
+    private Car testCar;
+    private Customer testCustomer;
+    private Principal testPrincipal;
+    private Order testOrder;
 
     //AA
     @BeforeEach
     public void setUp(){
         orderService = new OrderServiceImpl(mockOrderRepository, mockCarRepository, mockCustomerRepository);
 
-        testOrder.setId(1L);
-        testOrder.setCar();
-        testOrder.setCustomer();
-        testOrder.setBookedAt(LocalDate.of(2025,1,1));
-        testOrder.setIsActive(true);
-        testOrder.setStartDate(LocalDate.of(2025,4,1));
-        testOrder.setEndDate(LocalDate.of(2025,4,5));
-        testOrder.setTotalPrice();
+        testCar = new Car(10L, "Volvo", "V90","ABC789", CarStatus.AVAILABLE, BigDecimal.valueOf(1000), List.of());
+        testCustomer = new Customer(1L, "19890101-1234", "Anna", "Andersson", "anna@test.se", "070-1234567", "Solrosvägen 1, 90347 Umeå", List.of());
+        testOrder = new Order(1L, LocalDate.of(2025,1,1),LocalDate.now(),LocalDate.now().plusDays(5), testCar, testCustomer, BigDecimal.valueOf(5000),true);
+        testPrincipal = () -> testCustomer.getPersonalIdentityNumber();
     }
 
 
@@ -80,7 +70,7 @@ class OrderServiceImplUnitTest {
         when(mockOrderRepository.findById(testOrder.getId())).thenReturn(Optional.of(testOrder));
         when(mockOrderRepository.save(any(Order.class))).thenReturn(testOrder);
 
-        orderService.cancelOrder(1L, mockPrincipal);
+        orderService.cancelOrder(1L, testPrincipal);
 
         assertFalse(testOrder.getIsActive());
     }
@@ -88,7 +78,20 @@ class OrderServiceImplUnitTest {
     //AA
     @Test
     void cancelOrderShouldSetCorrectCancellationFee(){
+        when(mockOrderRepository.findById(testOrder.getId())).thenReturn(Optional.of(testOrder));
+        when(mockOrderRepository.save(any(Order.class))).thenReturn(testOrder);
 
+        orderService.cancelOrder(1L, testPrincipal);
+        long orderDaysLong = 5L;
+
+        BigDecimal originalPrice = testOrder.getTotalPrice();
+        orderService.cancelOrder(1L, testPrincipal);
+
+        BigDecimal expectedFee = originalPrice
+                .multiply(BigDecimal.valueOf(0.05))
+                .multiply(BigDecimal.valueOf(orderDaysLong));
+
+        assertEquals(0, expectedFee.compareTo(testOrder.getTotalPrice()));
     }
 
     //AA
@@ -97,7 +100,7 @@ class OrderServiceImplUnitTest {
         when(mockOrderRepository.findById(testOrder.getId())).thenReturn(Optional.of(testOrder));
         when(mockOrderRepository.save(any(Order.class))).thenReturn(testOrder);
 
-        String returnMessage = orderService.cancelOrder(1L, mockPrincipal);
+        String returnMessage = orderService.cancelOrder(1L, testPrincipal);
 
         assertEquals("Order with id '1' is cancelled", returnMessage);
     }
@@ -108,7 +111,7 @@ class OrderServiceImplUnitTest {
         Long missingOrderId = -1L;
         when(mockOrderRepository.findById(missingOrderId)).thenReturn(Optional.empty());
 
-        ResourceNotFoundException e = assertThrows(ResourceNotFoundException.class, () -> orderService.cancelOrder(missingOrderId, mockPrincipal));
+        ResourceNotFoundException e = assertThrows(ResourceNotFoundException.class, () -> orderService.cancelOrder(missingOrderId, testPrincipal));
 
         assertEquals("Order not found with id: " + missingOrderId, e.getMessage());
     }
@@ -116,7 +119,29 @@ class OrderServiceImplUnitTest {
     //AA
     @Test
     void cancelOrderShouldReturnMessageIfNoOrderWithThatIdExist(){
+        Long orderId = 1L;
 
+        Customer otherCustomer = new Customer(
+                2L, "20000101-9999", "Ove", "Olsson", "ove@fake.se", "070-1111111", "Falskgatan 1", List.of()
+        );
+
+
+        Order foreignOrder = new Order(
+                orderId,
+                LocalDate.now().plusDays(1),
+                LocalDate.now().plusDays(3),
+                LocalDate.now(),
+                testCar,
+                otherCustomer,
+                BigDecimal.valueOf(2000),
+                true
+        );
+
+        when(mockOrderRepository.findById(orderId)).thenReturn(Optional.of(foreignOrder));
+
+        String returnMessage = orderService.cancelOrder(orderId, testPrincipal);
+
+        assertEquals("No order for '19890101-1234' with id: 1", returnMessage);
     }
 
     //AA
