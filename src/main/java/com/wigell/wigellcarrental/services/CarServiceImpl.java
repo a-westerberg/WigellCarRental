de770d50779c7561accf38a333220dca930375b1
Sara <sara.andersson185@gmail.com>
2025-05-06 15:07:28 +0200

    WIG-126-SA * Added attribute on Order for isCancelled * In cancelled order did I change it so it also sets an order to isCancelled=true * In incomeCar() I changed so it only counts order where the car has been rented and where the startDate has happened

--- src/main/java/com/wigell/wigellcarrental/services/CarServiceImpl.java
+++ src/main/java/com/wigell/wigellcarrental/services/CarServiceImpl.java
@@ -111,11 +111,15 @@ public class CarServiceImpl implements CarService{
 
         for (Car car : cars) {
             BigDecimal totalIncome = BigDecimal.valueOf(0);
+            int rentedTimes = 0;
             for (Order order : car.getOrders()) {
-                totalIncome = totalIncome.add(order.getTotalPrice());
+                if(!order.getIsCancelled() && order.getStartDate().isBefore(LocalDate.now())) {
+                    rentedTimes++;
+                    totalIncome = totalIncome.add(order.getTotalPrice());
+                }
             }
 
-            incomeCars.add(new IncomeCar(car,car.getOrders().size(),totalIncome));
+            incomeCars.add(new IncomeCar(car,rentedTimes,totalIncome));
         }
         return incomeCars;
     }
