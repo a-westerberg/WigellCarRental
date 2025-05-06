941e78e83c8f047424787f54a31a62c87570ff79
Sommar-skog <amandaaronsson@hotmail.com>
2025-05-06 11:58:43 +0200

    CustomerControllerAndOrderServiceAndOrderRepositoryIntegrationTest.java

---
+++ src/test/java/com/wigell/wigellcarrental/controllers/CustomerControllerAndOrderServiceAndOrderRepositoryIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.wigell.wigellcarrental.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerControllerAndOrderServiceAndOrderRepositoryIntegrationTest {
+
+    @Test
+    void cancelOrder() {
+    }
+}
